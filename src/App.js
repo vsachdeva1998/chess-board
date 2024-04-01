722697efd88d93b0dacafd5b8b7314c117d94aed
Vinay <vsahdeva1998@gmail.com>
2024-04-01 21:36:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
+import React, { useState, useEffect, useMemo } from 'react';
 import './App.css';
 
-function App() {
+const ChessSquare = ({ isBlack }) => {
+  const color = isBlack ? 'black' : 'white';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        width: '100px',
+        height: '100px',
+        backgroundColor: color,
+      }}
+    ></div>
+  );
+};
+
+const App = () => {
+  const [dimension, setDimension] = useState(0);
+  const [chess, setChess] = useState([]);
+
+  const makeChessBoard = (dim) => {
+    let arr = [];
+    for (let i = 0; i < dim; i++) {
+      let temp = [];
+      for (let j = 0; j < dim; j++) {
+        temp.push((i + j) % 2 !== 0);
+      }
+      arr.push(temp);
+    }
+    return arr;
+  };
+
+  const memoizedChessboard = useMemo(() => makeChessBoard(dimension), [
+    dimension,
+  ]);
+
+  useEffect(() => {
+    setChess(memoizedChessboard);
+  }, [memoizedChessboard]);
+
+  return (
+    <div className='chess'>
+      <div>
+        <h2>
+          <span>NXN</span>
+          ChessBoard
+        </h2>
+        <input
+          type='number'
+          placeholder='Enter the Dimension'
+          onChange={(e) => setDimension(parseInt(e.target.value))}
+        />
+      </div>
+      <section
+        style={{
+          width: 100 * dimension,
+          display: 'flex',
+          flexWrap: 'wrap',
+          marginTop: '20px',
+          boxShadow: `0px 10px 10px rgba(0,0,0.1)`,
+        }}
+      >
+        {chess.map((row, rowIndex) => (
+          <div key={rowIndex} style={{ display: 'flex' }}>
+            {row.map((isBlack, colIndex) => (
+              <ChessSquare key={colIndex} isBlack={isBlack} />
+            ))}
+          </div>
+        ))}
+      </section>
     </div>
   );
-}
+};
 
 export default App;
